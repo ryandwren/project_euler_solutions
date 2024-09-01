bfac2669c711fec9b8e2651c7f6269c0f44001d9
ryandwren <ryandwren@gmail.com>
2024-09-01 10:02:05 -0400

    Display answers in a list

--- app/src/main/java/com/ryandwren/project_euler_solutions/MainScreen.kt
+++ app/src/main/java/com/ryandwren/project_euler_solutions/MainScreen.kt
@@ -1,9 +1,17 @@
 package com.ryandwren.project_euler_solutions
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,38 +22,71 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ryandwren.project_euler_solutions.problems.solveTotalMultiples
 
 @Composable
 fun MainScreen(){
-    var answer: Int? by remember{ mutableStateOf(null) }
 
-    Column(
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
-            onClick = {
-                answer = solveTotalMultiples(
-                    firstMultiple = 3,
-                    secondMultiple = 5,
-                    limitNumber = 1000
+        item {
+            var expanded by remember { mutableStateOf(false) }
+            Row (
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .clickable {
+                        expanded = !expanded
+                    }
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Problem 1: Multiples of 3 or 5",
+                    modifier = Modifier.weight(1f)
                 )
-            },
-            content = {
-                Text(text = "Problem 1" )
+
+                Image(
+                    painter = painterResource(id = R.drawable.nav_arrow_down),
+                    contentDescription = "min",
+                    modifier = if (expanded) Modifier.rotate(180f) else Modifier
+                )
             }
-        )
-        AnimatedVisibility(visible = (answer != null)) {
-            Text(text = answer.toString())
+
+            AnimatedVisibility(visible = expanded) {
+                var answer: Int? by remember{ mutableStateOf(null) }
+                Column {
+                    Button(
+                        onClick = {
+                            answer = solveTotalMultiples(
+                                firstMultiple = 3,
+                                secondMultiple = 5,
+                                limitNumber = 1000
+                            )
+                        },
+                        content = {
+                            Text(text = "Calculate" )
+                        }
+                    )
+                    AnimatedVisibility(visible = (answer != null)) {
+                        SelectionContainer {
+                            Text(text = answer.toString())
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
-@PreviewLightDark
+@Preview(showBackground = true)
+@Preview(showBackground = false, name = "Dark", uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL)
 fun PreviewMainScreen(){
     MaterialTheme{
         MainScreen()
